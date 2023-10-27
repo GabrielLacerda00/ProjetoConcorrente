74fe64f816799167788cd3df2d0affd4edc5f059
carlospamplona <carlos.filho@lsd.ufcg.edu.br>
2023-10-27 08:12:55 -0300

    add automation script

---
+++ conc/automate-busy_loop-vs-semaforo.sh
@@ -0,0 +1,13 @@
+sudo apt-get install update
+sudo apt-get install python3, pip, rustc
+
+pip install pandas
+pip install matplotlib
+
+chmod +x ./create_bakeryTest.sh
+./create_bakeryTest.sh
+
+chmod +x ./create_semaforoTest.sh
+./create_semaforoTest.sh
+
+python3 barrasV2.py
